8971b8c1e2af6e2930470236ebb5a5b0eac1bf10
jemmyalice <pjjemmy@gmail.com>
2025-01-01 13:54:07 +0800

    loss

--- ultralytics/train.py
+++ ultralytics/train.py
@@ -24,10 +24,9 @@ if __name__=='__main__':
 
     # 22_2 是去18基础去norm 29
     # 22 是22——2基础上加tff 29
-    # model = YOLO(r'F:\yolo_change_try\ultralytics-main\ultralytics\cfg\models\11\Fusion13_1_1212_yolo11s.yaml')
+    model = YOLO(r'F:\yolo_change_try\ultralytics-main\ultralytics\cfg\models\11\Fusion13_1212_yolo11s.yaml')
     # model = YOLO(r'F:\ultralytics-main\ultralytics\cfg\models\11\SKbase11_fusion_yolo11s.yaml')
-    # A0 25    B0 17   A1 25   B1 25(True) 13——1 25
-    model = YOLO(r'F:\yolo_change_try\ultralytics-main\ultralytics\cfg\models\11\Ayolo11s.yaml')
+    # model = YOLO(r'F:\yolo_change_try\ultralytics-main\ultralytics\cfg\models\11\Ayolo11s.yaml')
 
     state_dict = model.model.state_dict()
     i = 0
