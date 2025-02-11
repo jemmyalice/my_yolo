21f47c67532917c03c3e8a6c7bb0f9d857d3f884
jemmyalice <pjjemmy@gmail.com>
2025-02-11 12:12:50 +0800

    6661

--- ultralytics/nn/AddModules/__init__.py
+++ ultralytics/nn/AddModules/__init__.py
@@ -59,5 +59,6 @@ from .Add_Neck1 import * # 3
 from .Add_Neck2 import * # 4
 
 from .GFusion13_1212 import *
+from .G1Fusion13_1212 import *
 from .FE_Fusion13_1212 import *
 from .FE1_Fusion13_1212 import *
